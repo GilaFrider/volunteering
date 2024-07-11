b3802d392d361b9f40996f1e19b00203f7babd46
unknown <g0548457103@gmail.com>
2024-07-11 19:17:56 +0300

    Delete unnecessary file

--- S.O.SProject/repositories/FieldRepo.js
+++
@@ -1,8 +0,0 @@
-import FieldModel from "../models/FieldModel.js";
-import BaseRepo from "./BaseRepo.js";
-class FieldRepo extends BaseRepo {
-    constructor(model) {
-        super(model);
-    }
-}
-export default new FieldRepo(FieldModel);
